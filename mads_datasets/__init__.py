77ef7e64f190e9cff8343d6bbc594f014888ac04
R.Grouls <rgrouls@gmail.com>
2023-06-12 09:07:26 +0200

    added __version__, clean up dependencies

--- mads_datasets/__init__.py
+++ mads_datasets/__init__.py
@@ -2,3 +2,5 @@ from mads_datasets.datasetfactory import DatasetFactoryProvider
 from mads_datasets.settings import DatasetType
 
 __all__ = ["DatasetFactoryProvider", "DatasetType"]
+
+__version__ = "0.1.5.2"
