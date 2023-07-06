90a1491b0aac2740b51a9f5068dd52cb69e3a3e7
R.Grouls <rgrouls@gmail.com>
2023-07-06 16:16:14 +0200

    factory to init

--- mads_datasets/__init__.py
+++ mads_datasets/__init__.py
@@ -1,6 +1,48 @@
-from mads_datasets.datasetfactory import DatasetFactoryProvider
-from mads_datasets.settings import DatasetType
+from pathlib import Path
+
+from mads_datasets.base import AbstractDatasetFactory
+from mads_datasets.factories import (
+    FashionDatasetFactory,
+    FlowersDatasetFactory,
+    GesturesDatasetFactory,
+    IMDBDatasetFactory,
+    IrisDatasetFactory,
+    PenguinsDatasetFactory,
+    SunspotsDatasetFactory,
+)
+from mads_datasets.settings import (
+    DatasetType,
+    fashionmnistsettings,
+    flowersdatasetsettings,
+    gesturesdatasetsettings,
+    imdbdatasetsettings,
+    irissettings,
+    penguinssettings,
+    sunspotsettings,
+)
 
 __all__ = ["DatasetFactoryProvider", "DatasetType"]
 
-__version__ = "0.1.6"
+__version__ = "0.2"
+
+
+class DatasetFactoryProvider:
+    @staticmethod
+    def create_factory(dataset_type: DatasetType, **kwargs) -> AbstractDatasetFactory:
+        datadir = Path(kwargs.get("datadir", Path.home() / ".cache/mads_datasets"))
+        if dataset_type == DatasetType.FLOWERS:
+            return FlowersDatasetFactory(flowersdatasetsettings, datadir=datadir)
+        if dataset_type == DatasetType.IMDB:
+            return IMDBDatasetFactory(imdbdatasetsettings, datadir=datadir)
+        if dataset_type == DatasetType.GESTURES:
+            return GesturesDatasetFactory(gesturesdatasetsettings, datadir=datadir)
+        if dataset_type == DatasetType.FASHION:
+            return FashionDatasetFactory(fashionmnistsettings, datadir=datadir)
+        if dataset_type == DatasetType.SUNSPOTS:
+            return SunspotsDatasetFactory(sunspotsettings, datadir=datadir)
+        if dataset_type == DatasetType.IRIS:
+            return IrisDatasetFactory(irissettings, datadir=datadir)
+        if dataset_type == DatasetType.PENGUINS:
+            return PenguinsDatasetFactory(penguinssettings, datadir=datadir)
+
+        raise ValueError(f"Invalid dataset type: {dataset_type}")
